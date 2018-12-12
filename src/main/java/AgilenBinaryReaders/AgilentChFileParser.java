0772d506728b8b3f7e1ab7b4db6c03eeafb97325
mercenery <mercenery@inbox.ru>
2018-12-13 00:23:38 +0300

    beautify

--- src/main/java/AgilenBinaryReaders/AgilentChFileParser.java
+++ src/main/java/AgilenBinaryReaders/AgilentChFileParser.java
@@ -6,64 +6,63 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 
 public class AgilentChFileParser {
+    
     // utility for convert
     final static char[] hexArray = "0123456789ABCDEF".toCharArray();
-
+    final static String defaultChFilePath = "C:\\work\\OpenChrome\\Sample 1\\002-1401.D\\DAD1A.ch";
+    final static String defaultOutputFile = "C:\\work\\DAD1A_TWO_COLOMNS.csv";
+    final static String header = "Time(ms),Intensity(abs)\n";
+    final static char[] headerCharArray = header.toCharArray();
+    
+    
     public static void main(String[] args) throws IOException, InterruptedException {
         //resources opening
-        File fileOut = new File("C:\\work\\DAD1A_TWO_COLOMNS.csv");
+        File fileOut = new File(args[1] == null ? defaultOutputFile : args[1]);
         fileOut.createNewFile();
-
-        RandomAccessFile
-                randomAccessFile =
-                new RandomAccessFile("C:\\work\\OpenChrome\\Sample 1\\002-1401.D\\DAD1A.ch", "r");
-        String header = "Time(ms),Intensity(abs)\n";
+    
+        RandomAccessFile randomAccessFile = new RandomAccessFile(args[0] == null ? defaultChFilePath : args[0], "r");
         byte[] intensityAbsolutBuffer = new byte[2];
-
+    
         // time scale start value set
         int timeRespectToIntensityPoint = 325;
         // step of intensity abs set
         int intensityValue = 0;
         // time scale step set
         int timeDeltaIncrease = 400;
-
+    
         // start of valuable part of table hex = 1802, dec = 6146
         int intensityAbsolutOffsetPoint = 6146;
         // intensity steps set
         int intensityDeltaIncrease = 2;
-
-        System.out.println(header);
-        char[] headerCharArray = header.toCharArray();
+    
         try (FileOutputStream fileOutputStream = new FileOutputStream(fileOut)) {
-
-            // header creation
+            // header infile creation
             for (int i = 0; i < headerCharArray.length; i++) {
                 fileOutputStream.write(headerCharArray[i]);
             }
-
+        
             // temp var set
             long randomFileLength = randomAccessFile.length();
             System.out.println("randomFileLength  = " + randomFileLength);
             int randomFileLengthCount = 0;
-
+        
             // main loop start until counter less than .ch file length
             while (randomFileLengthCount < 12181) {
-
+    
                 // file length counter set
                 randomFileLengthCount++;
-
+    
                 // create new line for output
                 String newResultTimeMsIntensityAbsString = "";
-
+    
                 // reader pointer in source .ch file set
                 randomAccessFile.seek(intensityAbsolutOffsetPoint);
-
+    
                 // read bytes of intensity value from set position into temporary buffer
                 randomAccessFile.read(intensityAbsolutBuffer, 0, intensityAbsolutBuffer.length);
                 String newIntensityLine = "";
-                short
-                        currentIntensityStepValue =
-                        (short) hex2Decimal(getHexStringFromByteArray(intensityAbsolutBuffer));
+                short currentIntensityStepValue
+                        = (short) hex2Decimal(getHexStringFromByteArray(intensityAbsolutBuffer));
                 intensityValue = intensityValue + currentIntensityStepValue;
                 newIntensityLine = "" + intensityValue;
                 // prepare time point value inside new string for output file
@@ -80,8 +79,9 @@ public class AgilentChFileParser {
             randomAccessFile.close();
         }
     }
-
+    
     public static String getHexStringFromByteArray(byte[] bytes) {
+        
         char[] hexChars = new char[bytes.length * 2];
         for (int j = 0; j < bytes.length; j++) {
             int v = bytes[j] & 0xFF;
@@ -90,16 +90,18 @@ public class AgilentChFileParser {
         }
         return new String(hexChars);
     }
-
+    
     public static int getDecimalFromHexByteArray(byte[] bytes) {
+        
         int result = 0;
         for (int i = 0; i < bytes.length; i++) {
             result = result | (bytes[i] & 0xff) >> (i * 8);
         }
         return result;
     }
-
+    
     public static int hex2Decimal(String s) {
+        
         String digits = "0123456789ABCDEF";
         s = s.toUpperCase();
         int val = 0;
