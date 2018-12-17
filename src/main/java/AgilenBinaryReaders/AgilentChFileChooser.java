0d6affe1b95306c9620154a0fb7e054120c63af1
Oleg_Fedulov@epam.com <Oleg_Fedulov@epam.com>
2018-12-17 11:34:41 +0300

    gui works

--- src/main/java/AgilenBinaryReaders/AgilentChFileChooser.java
+++ src/main/java/AgilenBinaryReaders/AgilentChFileChooser.java
@@ -1,63 +1,17 @@
 package AgilenBinaryReaders;
 
 import javax.swing.*;
-import java.io.IOException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class AgilentChFileChooser extends JFrame {
-    private final JTextArea outputArea; // displays file contents
 
     // set up GUI
-    public AgilentChFileChooser() throws Exception {
-        super("JFileChooser Demo");
-        outputArea = new JTextArea();
-        add(new JScrollPane(outputArea)); // outputArea is scrollable
-        analyzePath(); // get Path from user and display info
+    public AgilentChFileChooser() {
+	    super("AgilentChFileChooser");
     }
 
-    // display information about file or directory user specifies
-    public void analyzePath() throws IOException {
-        // get Path to user-selected file or directory
-        Path path = getFileOrDirectoryPath();
-
-        if (path != null && Files.exists(path)) // if exists, display info
-        {
-            // gather file (or directory) information
-            StringBuilder builder = new StringBuilder();
-            builder.append(String.format("%s:%n", path.getFileName()));
-            builder.append(String.format("%s a directory%n", Files.isDirectory(path) ? "Is" : "Is not"));
-            builder.append(String.format("%s an absolute path%n", path.isAbsolute() ? "Is" : "Is not"));
-            builder.append(String.format("Last modified: %s%n", Files.getLastModifiedTime(path)));
-            builder.append(String.format("Size: %s%n", Files.size(path)));
-            builder.append(String.format("Path: %s%n", path));
-            builder.append(String.format("Absolute path: %s%n", path.toAbsolutePath()));
-
-            if (Files.isDirectory(path)) // output directory listing
-            {
-                builder.append(String.format("%nDirectory contents:%n"));
-
-                // object for iterating through a directory's contents
-                DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path);
-
-                for (Path p : directoryStream) {
-                    builder.append(String.format("%s%n", p));
-                }
-            }
-
-            outputArea.setText(builder.toString()); // display String content
-        } else // Path does not exist
-        {
-            JOptionPane.showMessageDialog(this,
-                                          path.getFileName() + " does not exist.",
-                                          "ERROR",
-                                          JOptionPane.ERROR_MESSAGE);
-        }
-    } // end method analyzePath
-
     // allow user to specify file or directory name
-    private Path getFileOrDirectoryPath() {
+    public Path getFileOrDirectoryPath() {
         // configur dialog allowing selection of a file
         JFileChooser fileChooser = new JFileChooser();
         fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
